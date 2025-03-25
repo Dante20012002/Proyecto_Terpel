7f322f7f9174b563c4b1ac25958fc00772b7c9cf
dante20012002 <santiagod.puentesg@gmail.com>
2025-03-25 16:26:30 -0500

    hots config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   
   vite: {
     server: {
-      allowedHosts: ['http://127.0.0.1:5500/'], // permisos de dominio
+      allowedHosts: ['https://dante20012002.github.io/Proyecto_Terpel/'], // permisos de dominio
     },
     
   },
--- src/scripts/pdf.js
+++ src/scripts/pdf.js
@@ -1,6 +1,13 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-console.log("Iniciando lenctura");
-var files =fs.readdirSync("../../public/media/docs/Boletines");
-console.log("Finalizando lectura");
-console.log(files)
+// console.log("Iniciando lenctura");
+// var files =fs.readdirSync("../../public/media/docs/Boletines");
+// console.log("Finalizando lectura");
+// console.log(files)
+
+function suma(a) {
+  return a + 2;
+}
+
+let resultado = suma(5);
+console.log("El resultado de la suma es ");
