060c78d980bb9c864663ae8fba85c7cf3ef6b09f
Dante20012002 <santiagod.puentesg@gmail.com>
2025-04-30 17:08:30 -0500

    Cambio de pc

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,18 +1,20 @@
 import { renderers } from './renderers.mjs';
-import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_j3VBbFwJ.mjs';
-import { manifest } from './manifest_B8PXZkGn.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CNPx3fPY.mjs';
+import { manifest } from './manifest_B0JRd_H7.mjs';
 
 const serverIslandMap = new Map();;
 
-const _page0 = () => import('./pages/antioquia_prog.astro.mjs');
-const _page1 = () => import('./pages/boletin_qnlp.astro.mjs');
-const _page2 = () => import('./pages/programacion_escuelascol.astro.mjs');
-const _page3 = () => import('./pages/index.astro.mjs');
+const _page0 = () => import('./pages/acerca_escuelas.astro.mjs');
+const _page1 = () => import('./pages/antioquia_prog.astro.mjs');
+const _page2 = () => import('./pages/boletin_qnlp.astro.mjs');
+const _page3 = () => import('./pages/programacion_escuelascol.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
-    ["src/pages/antioquia_prog.astro", _page0],
-    ["src/pages/boletin_qnlp.astro", _page1],
-    ["src/pages/programacion_escuelascol.astro", _page2],
-    ["src/pages/index.astro", _page3]
+    ["src/pages/acerca_escuelas.astro", _page0],
+    ["src/pages/antioquia_prog.astro", _page1],
+    ["src/pages/boletin_qnlp.astro", _page2],
+    ["src/pages/programacion_escuelascol.astro", _page3],
+    ["src/pages/index.astro", _page4]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -23,8 +25,8 @@ const _manifest = Object.assign(manifest, {
 });
 const _args = {
     "mode": "standalone",
-    "client": "file:///G:/Proyecto%20Terpel%20V2/dist/client/",
-    "server": "file:///G:/Proyecto%20Terpel%20V2/dist/server/",
+    "client": "file:///C:/Users/santi/Proyecto_Terpel/dist/client/",
+    "server": "file:///C:/Users/santi/Proyecto_Terpel/dist/server/",
     "host": false,
     "port": 4321,
     "assets": "_astro"
